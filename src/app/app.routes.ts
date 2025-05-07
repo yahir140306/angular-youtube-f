d49ff473325b79e56961345c34a66d8de54a7b84
yahir140306 <joseyahirhernandez11@gmail.com>
2025-05-06 23:17:10 -0600

    agregar ruta para la página de Dragonball

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,9 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroPageComponent
+  },{
+    path: 'dragonball',
+    component: DragonballPageComponent
   },
   {
     path: '**',
