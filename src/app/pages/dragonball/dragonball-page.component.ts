d2d4ec5ef4b8f2d78c48aa8ac3c8cb06d127efdf
yahir140306 <joseyahirhernandez11@gmail.com>
2025-05-07 22:58:40 -0600

    mejorar la página de DragonBall: agregar funcionalidad para agregar personajes y mostrar poderes

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -9,13 +10,41 @@ interface Character {
 @Component({
   selector: 'app-dragonball',
   standalone: true,
+  // imports: [ NgClass],
   imports: [],
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
     { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
+
+  // powerClass = computed( () => {
+  //   return {
+  //     'text-danger': true,
+  //   };
+  // });
+
+  addCharacter() {
+    console.log(this.name(), this.power());
+
+    if ( !this.name() || !this.power() || this.power() <= 0 ) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    }
+
+    this.characters().push(newCharacter);
+
+  }
 }
