0753bf444bdba9167253c23b3f98f673111937cb
yahir140306 <joseyahirhernandez11@gmail.com>
2025-05-14 23:28:20 -0600

    refactor: reiniciar valores de nombre y poder en el componente Dragonball

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -15,14 +15,14 @@ interface Character {
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
-  name = signal('Gohan');
-  power = signal(100);
+  name = signal('');
+  power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 3, name: 'Piccolo', power: 3000 },
-    { id: 4, name: 'Yamcha', power: 500 },
+    // { id: 2, name: 'Vegeta', power: 8000 },
+    // { id: 3, name: 'Piccolo', power: 3000 },
+    // { id: 4, name: 'Yamcha', power: 500 },
   ]);
 
   // powerClass = computed( () => {
