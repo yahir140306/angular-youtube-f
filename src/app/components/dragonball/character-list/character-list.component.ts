f6baa03b99125e398e54dfc4a1296074363b643b
yahir140306 <joseyahirhernandez11@gmail.com>
2025-05-14 23:28:26 -0600

    agregar componente de lista de personajes para Dragonball

---
+++ src/app/components/dragonball/character-list/character-list.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'dragon-character-list',
+  templateUrl: './character-list.component.html',
+})
+export class CharacterListComponent {
+characters: any;
+}
