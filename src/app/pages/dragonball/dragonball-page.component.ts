3085f6c11795db000f573df9e3f644d4d10b08ee
yahir140306 <joseyahirhernandez11@gmail.com>
2025-05-16 22:43:40 -0600

    refactor: simplificar la sección de adición de personajes en la página de DragonBall

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -20,9 +20,9 @@ export class DragonballPageComponent {
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    // { id: 2, name: 'Vegeta', power: 8000 },
-    // { id: 3, name: 'Piccolo', power: 3000 },
-    // { id: 4, name: 'Yamcha', power: 500 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
 
   // powerClass = computed( () => {
