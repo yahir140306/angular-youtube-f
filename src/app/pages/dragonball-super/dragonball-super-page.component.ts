f0b34156ea6ad66b7fae1708adc1954b4aad9e97
yahir140306 <joseyahirhernandez11@gmail.com>
2025-05-16 22:43:46 -0600

    refactor: actualizar la importación de componentes y simplificar la estructura del componente DragonballSuperPage

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,46 +1,22 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
-
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { Character } from '../../interfaces/character.interface';
+import { DragonballService } from '../service/dragonball.service';
 
 @Component({
   selector: 'dragonball-super',
-  imports: [CharacterListComponent],
+  imports: [CharacterListComponent, CharacterAddComponent],
   templateUrl: './dragonball-super-page.component.html',
-  standalone: true
+  standalone: true,
 })
 export class DragonballSuperPageComponent {
-  name = signal('');
-  power = signal(0);
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
+  // constructor(
+  //   public dragonballService: DragonballService
+  // ) {}
 
-  addCharacter() {
-    console.log(this.name(), this.power());
+  public dragonBallService = inject(DragonballService);
 
-    if ( !this.name() || !this.power() || this.power() <= 0 ) {
-      return;
-    }
 
-    const newCharacter: Character = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power(),
-    }
-
-    this.characters.update( (list) => [...list, newCharacter]);
-    this.resetFields();
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
 }
